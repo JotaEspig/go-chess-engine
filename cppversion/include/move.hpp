3fea79e3eec4d7ddb148e6e85da84da619db1a7a
João Vitor Espig <jotaespig@gmail.com>
2025-02-15 01:17:02 -0300

    bla

--- cppversion/include/move.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-struct Move {
-    uint64_t from = 0;
-    uint64_t to = 0;
-};
--- cppversion/include/utils.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-std::vector<std::string> split(const std::string &s, char delim);
-
-int getPieceIdx(char c);
-char getPieceChar(int i, bool isWhite);
